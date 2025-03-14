95b4c526307077f9c0f5d138e96ee3c250dee51d
elde-n <elden_@tutanota.com>
2025-03-14 17:59:20 +0200

    initial release

---
+++ src/bin/bundle/lua.lua
@@ -0,0 +1,112 @@
+local functions = {}
+functions.__index = functions
+
+---@class Functions
+---@field context Context
+
+---@class Context
+---@field file_path string
+---@field files table<string>
+---@field modules table<string, function>	
+
+---@param context Context
+---@return Functions
+function functions.new(context)
+	return setmetatable({ context = context }, functions)
+end
+
+---@param content string
+---@param separator string?
+---@return table<string>
+function functions:string_split(content, separator)
+	local result = {}
+	for v in string.gmatch(content, "([^" .. separator .. "]+)") do
+		table.insert(result, v)
+	end
+
+	return result
+end
+
+---@param file string
+---@return string
+function functions:get_file_path(file)
+	local result = {}
+	for _, v in next, self:string_split(file, '/') do
+		if v == ".." then
+			table.remove(result)
+		elseif v ~= '.' then
+			table.insert(result, v)
+		end
+	end
+
+	return table.concat(result, '/')
+end
+
+---@return string
+function functions:get_caller_directory()
+	local caller = self.context.file_path
+	if not caller then return '' end
+
+	local path = self:string_split(caller, '/')
+	table.remove(path)
+
+	local directory = table.concat(path, '/')
+	if #directory == 0 then return '' end
+
+	return directory .. '/'
+end
+
+---@param directory string
+---@return string
+function functions:remove_trailing_slash(directory)
+	if string.sub(directory, 1, -1) == '/' then
+		return string.sub(directory, 1, -2)
+	end
+
+	return directory
+end
+
+---@param path string
+---@return string?, string?
+function functions:get_module_from_relative_path(path)
+	local file = self:get_file_path(self:get_caller_directory() .. path)
+	file = self:remove_trailing_slash(file)
+
+	if self.context.files[file] then return file, file end
+	if self.context.files[file .. "/init"] then
+		return file .. '/', file .. "/init"
+	end
+end
+
+---@param module string?
+---@return any?
+function functions:require(module)
+	if type(module) == "string" then
+		local _name, file = self:get_module_from_relative_path(module)
+		if not file then
+			_name, file = self:get_module_from_global_path(module)
+		end
+
+		if not file then
+			error(string.format("could not load module: %s, module was not found", module))
+		end
+
+		if not self.context.modules[file] then
+			self.context.modules[file] = self.context.files[file](functions.new({
+				file_path = file,
+				files = self.context.files,
+				modules = self.context.modules
+			}))
+		end
+
+		return self.context.modules[file]
+	end
+end
+
+---@param functions table
+---@return function
+local function get_require(functions)
+	return function(...)
+		return functions:require(...)
+	end
+end
---
+++ src/bin/bundle/main.rs
@@ -0,0 +1,243 @@
+use std::path::{Path, PathBuf};
+
+use toml;
+
+const BUILD_FILE: &str = "build.toml";
+const DEFAULT_REQUIRE_FUNCTION: &str = "require";
+
+enum LuaVersion {
+    Lua,
+    Luau,
+}
+
+struct Project {
+    name: String,
+    output: PathBuf,
+    entry_point: PathBuf,
+    files: Vec<PathBuf>,
+    lua_version: LuaVersion,
+}
+
+struct BuildFile {
+    projects: Vec<Project>,
+    require_function: String,
+}
+
+fn main() {
+    let Some(build) = parse_build_file() else {
+        return;
+    };
+
+    let require_method = build.require_function;
+
+    for project in build.projects {
+        let mut output = format!("{}", include_str!("lua.lua"));
+
+        output.push_str("\nlocal files = {");
+        for file in project.files {
+            let name = path_without_extension(&file).to_str().unwrap().into();
+            let content = std::fs::read_to_string(file).unwrap();
+            output.push_str(insert_module(name, content, require_method.clone(), 1).as_str());
+        }
+        output.push_str("\n}\n");
+
+        output.push_str(
+            insert_entry_point(
+                path_without_extension(&project.entry_point)
+                    .to_str()
+                    .unwrap()
+                    .into(),
+            )
+            .as_str(),
+        );
+
+        std::fs::create_dir_all(&project.output).unwrap();
+        std::fs::write(&project.output.join(project.name), output).unwrap();
+    }
+}
+
+fn parse_build_file() -> Option<BuildFile> {
+    if !std::fs::exists(BUILD_FILE).unwrap() {
+        println!("error: could not find `build.toml` file");
+        return None;
+    }
+
+    let build = std::fs::read_to_string(BUILD_FILE).unwrap();
+    let table = build.as_str().parse::<toml::Table>().unwrap();
+
+    let projects = match table.get("project") {
+        Some(value) => {
+            let mut projects = Vec::new();
+            let array = value.as_array().unwrap();
+
+            for value in array {
+                let table = value.as_table().unwrap();
+                let Some(project) = parse_project(table) else {
+                    continue;
+                };
+
+                projects.push(project);
+            }
+
+            projects
+        }
+        None => {
+            println!("error: missing [[project]] filed in build.toml");
+            return None;
+        }
+    };
+
+    Some(BuildFile {
+        projects,
+        require_function: DEFAULT_REQUIRE_FUNCTION.into(),
+    })
+}
+
+fn parse_project(table: &toml::Table) -> Option<Project> {
+    let name = format!(
+        "{}.lua",
+        match table.get("name") {
+            Some(value) => value.as_str().unwrap(),
+            None => "a",
+        }
+    );
+
+    let output = match table.get("output") {
+        Some(value) => value.as_str().unwrap(),
+        None => "build",
+    }
+    .into();
+
+    let entry_point = match table.get("entry_point") {
+        Some(value) => {
+            let entry = PathBuf::from(value.as_str().unwrap());
+            if !entry.exists() {
+                println!("error: a project entry contains an invalid file in the `entry_point`");
+                return None;
+            }
+            entry
+        }
+        None => {
+            println!("error: a project entry is missing a `entry_point` file");
+            return None;
+        }
+    }
+    .into();
+
+    let lua_version = match table.get("lua_version") {
+        Some(value) => LuaVersion::from(value.as_str().unwrap()),
+        None => LuaVersion::Lua,
+    };
+
+    let files = match table.get("files") {
+        Some(value) => {
+            let mut files = Vec::new();
+            let array = value.as_array().unwrap();
+            for value in array {
+                let entry = PathBuf::from(value.as_str().unwrap());
+                if !entry.exists() {
+                    println!("error: a project entry contains an invalid file in the `files` list");
+                    return None;
+                }
+
+                files.extend(files_from_path(&entry));
+            }
+
+            files
+        }
+
+        None => {
+            println!("error: a project entry is missing a `files` list");
+            return None;
+        }
+    };
+
+    Some(Project {
+        name,
+        output,
+        entry_point,
+        files,
+        lua_version,
+    })
+}
+
+fn files_from_path(path: &Path) -> Vec<PathBuf> {
+    let mut files = Vec::new();
+
+    if path.is_file() {
+        files.push(path.into());
+    }
+
+    if path.is_dir() {
+        for entry in path.read_dir().unwrap() {
+            let entry = entry.unwrap().path();
+            files.extend(files_from_path(&entry));
+        }
+    }
+
+    files
+}
+
+fn path_without_extension(path: &PathBuf) -> PathBuf {
+    let mut path = path.clone();
+    // TODO: use file_prefix
+    if let Some(stem) = path.clone().file_stem() {
+        path.set_file_name(stem);
+    }
+
+    path
+}
+
+fn indent_block(block: String, level: usize) -> String {
+    let lines: Vec<String> = block
+        .lines()
+        .map(|line| {
+            let indentation = "\t".repeat(level);
+            format!("{indentation}{line}")
+        })
+        .collect();
+
+    lines.join("\n")
+}
+
+fn inject_require(code: String, require: String) -> String {
+    format!(
+        "local {require}, functions, get_require = get_require(functions), nil, nil
+
+{code}"
+    )
+}
+
+fn insert_module(file: String, code: String, require: String, level: usize) -> String {
+    let code = indent_block(inject_require(code, require), 1);
+    indent_block(
+        format!(
+            "
+[\"{file}\"] = function(functions)
+{code}
+end,
+"
+        ),
+        level,
+    )
+}
+
+fn insert_entry_point(entry_point: String) -> String {
+    format!(
+        "
+functions.new({{
+    files = files,
+    modules = {{}},
+}}):require(\"{entry_point}\")"
+    )
+}
+
+impl From<&str> for LuaVersion {
+    fn from(string: &str) -> Self {
+        match string {
+            "Lua" => Self::Lua,
+            "Luau" => Self::Luau,
+            _ => Self::Lua,
+        }
+    }
+}
